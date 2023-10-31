a63e6e4918223e5e7e0dbd56eccfcae4307204cc
anyongjin <anyongjin163@163.com>
2023-10-31 21:17:32 +0800

    update config

--- config.ts
+++ config.ts
@@ -1,13 +1,19 @@
 import {type Period, type SymbolInfo} from "~/composables/types";
 import {PonentDatafeed} from "~/composables/kline/datafeeds";
 
-export const getDefaults = () => {
+export function getDefaults() {
   return {
     //symbol: {shortName: 'BTC/USDT.P', ticker: 'BTC/USDT.P', exchange: 'binance'} as SymbolInfo,
     //symbol: {shortName: 'ETH', name: 'ETH', ticker: 'ethereum', exchange: 'binance'} as SymbolInfo,
-    symbol: {shortName: '601318', name: 'SH:601318', ticker: '601318.SH', exchange: 'SH', title: '中国平安'} as SymbolInfo,
+    symbol: {
+      shortName: '601318',
+      name: 'SH:601318',
+      ticker: '601318.SH',
+      exchange: 'SH',
+      title: '中国平安'
+    } as SymbolInfo,
     Datafeed: PonentDatafeed,
-    period: { multiplier: 1, timespan: 'day', text: '1D', timeframe: '1d', secs: 86400 } as Period,
+    period: {multiplier: 1, timespan: 'day', text: '1D', timeframe: '1d', secs: 86400} as Period,
     maxBarNum: 5000,
     data_url: 'https://ponentsoft.com/api',
     // ponentsoft的验证token
